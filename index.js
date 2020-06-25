7e5b7f4ec41822957de81cba527ad79aad172688
Robert Wagner <rwwagner90@gmail.com>
2020-06-25 12:00:29 -0400

    Add initial iOS commands (#2)
    * Start iOS commands
    
    * Lowercase ios
    
    * Add ios:build command

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  includedCommands() {
+    return {
+      'ios:build': require('./lib/commands/ios-build'),
+      'ios:serve': require('./lib/commands/ios-serve')
+    };
+  },
+
+  isDevelopingAddon() {
+    return true;
+  }
 };
